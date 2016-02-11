951cdbba33d5fa8b01c61b81cd76f38fa890a9a5
Michael Nitschinger <michael@nitschinger.at>
2016-02-11 11:15:58 +0100

    more detailed support example

--- examples/support.rs
+++ examples/support.rs
@@ -6,11 +6,16 @@ use hwloc::Topology;
 fn main() {
     let topo = Topology::new();
 
-    // Check if CPU Binding is supported
-    println!("CPU Binding supported: {}", topo.support().cpu().set_process());
+    // Check if Process Binding for CPUs is supported
+    println!("CPU Binding (current process) supported: {}", topo.support().cpu().set_current_process());
+    println!("CPU Binding (any process) supported: {}", topo.support().cpu().set_process());
+
+    // Check if Thread Binding for CPUs is supported
+    println!("CPU Binding (current thread) supported: {}", topo.support().cpu().set_current_thread());
+    println!("CPU Binding (any thread) supported: {}", topo.support().cpu().set_thread());
 
     // Check if Memory Binding is supported
-    println!("Memory Binding supported: {}", topo.support().memory().set_process());
+    println!("Memory Binding supported: {}", topo.support().memory().set_current_process());
 
     // Debug Print all the Support Flags
     println!("All Flags:\n{:?}", topo.support());
