9a4076b3bd527aeb1e5d0f82dd9b88591703ff4e
Michael Nitschinger <michael@nitschinger.at>
2016-12-28 11:12:20 +0100

    don't fail if lib not found in pkg

--- build.rs
+++ build.rs
@@ -1,5 +1,8 @@
 extern crate pkg_config;
 
 fn main() {
-    pkg_config::Config::new().atleast_version("1.11.0").probe("hwloc").unwrap();
+    let probed = pkg_config::Config::new().atleast_version("1.11.0").probe("hwloc");
+    if probed.is_ok() {
+        return;
+    }
 }
